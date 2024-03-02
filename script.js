a73157c76219327113831bff564156d1127f9e80
Timéo <116905321+TimeoDoom@users.noreply.github.com>
2024-03-02 23:00:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const observer = new IntersectionObserver(handleIntersection, {
     root: null,
     rootMargin: "0px",
-    threshold: 1, // Change this threshold as needed
+    threshold: 0, // Change this threshold as needed
   });
 
   boxes.forEach((box) => {
     observer.observe(box);
+  });
+});
+
+
+const links = document.querySelectorAll("nav li");
+
+icons.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
   });
 });
