da4ca7cbcec3c9792c60233c3aedd24d2dd46030
Timéo <116905321+TimeoDoom@users.noreply.github.com>
2024-07-10 17:30:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,11 +27,11 @@ const closeIcone = document.getElementById("close");
 
 check.addEventListener("change", function () {
   if (this.checked) {
-    console.log("La checkbox est cochée.");
+    console.log("La checkbox est cochÃ©e.");
     menu.style.display = "none";
     closeIcone.style.display = "block";
   } else {
-    console.log("La checkbox est décochée.");
+    console.log("La checkbox est dÃ©cochÃ©e.");
     menu.style.display = "block";
     closeIcone.style.display = "none";
   }
@@ -46,3 +46,42 @@ links.forEach((link) => {
     nav.classList.remove("active");
   });
 });
+
+// Typing Animation
+
+let aText = new Array(
+  "Je suis Timéo,",
+  "passionné de développement web et mobile."
+);
+let iSpeed = 100; // time delay of print out
+let iIndex = 0; // start printing array at this posision
+let iArrLength = aText[0].length; // the length of the text array
+let iScrollAt = 20; // start scrolling up at this many lines
+
+let iTextPos = 0; // initialise text position
+let sContents = ""; // initialise contents variable
+let iRow; // initialise current row
+
+function typewriter() {
+  sContents = " ";
+  iRow = Math.max(0, iIndex - iScrollAt);
+  let destination = document.getElementById("typedtext");
+
+  while (iRow < iIndex) {
+    sContents += aText[iRow++] + "<br />";
+  }
+  destination.innerHTML =
+    sContents + aText[iIndex].substring(0, iTextPos) + "_";
+  if (iTextPos++ == iArrLength) {
+    iTextPos = 0;
+    iIndex++;
+    if (iIndex != aText.length) {
+      iArrLength = aText[iIndex].length;
+      setTimeout("typewriter()", 500);
+    }
+  } else {
+    setTimeout("typewriter()", iSpeed);
+  }
+}
+
+typewriter();
