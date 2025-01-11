b8dcf64412e4d1fd63e56002d1d26e3b742accf3
Timéo <116905321+TimeoDoom@users.noreply.github.com>
2025-01-11 13:33:22 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+document.querySelectorAll(".nav-item").forEach((item) => {
+  const dot = item.querySelector(".dot");
+
+  item.addEventListener("mousemove", (event) => {
+    const rect = item.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = 50; // Ajustement pour correspondre à la nouvelle position du point
+
+    const distX = (x - centerX) / 2; // Augmentation de la sensibilité horizontale
+    const distY = (y - centerY) / 2; // Augmentation de la sensibilité verticale
+
+    dot.style.transform = `translate(${distX}px, ${distY}px)`;
+  });
+
+  item.addEventListener("mouseleave", () => {
+    dot.style.transform = `translateX(-50%)`;
+  });
+});
+
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
+const themeIcon = document.getElementById("icon");
+
+const enableDarkmode = () => {
+  document.body.classList.add("darkmode");
+  themeIcon.src = "../assets/sun.svg";
+  localStorage.setItem("darkmode", "active");
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove("darkmode");
+  themeIcon.src = "../assets/moon.svg";
+  localStorage.setItem("darkmode", "null");
+};
+
+if (darkmode === "active") enableDarkmode();
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
